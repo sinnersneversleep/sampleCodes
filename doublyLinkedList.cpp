e1cb635f83ab2c0ab6056b859530ab557b96845c
sinnersneversleep <shubhamswaraj900@gmail.com>
2023-08-25 01:38:20 +0530

    doubly linked list

---
+++ doublyLinkedList.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    void insertFront(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void insertBack(int value) {
+        Node* newNode = new Node(value);
+        if (!tail) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList dll;
+    dll.insertFront(3);
+    dll.insertFront(2);
+    dll.insertFront(1);
+    dll.insertBack(4);
+    dll.insertBack(5);
+
+    std::cout << "Doubly Linked List: ";
+    dll.display();
+
+    return 0;
+}
